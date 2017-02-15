fc36aec502d82c4f1d62a5e39f9cf17fdb8a50f4
Naveen Muthu <muthun@email.chop.edu>
2017-02-15 14:57:43 -0500

    Making institution foreign key

--- djangoapp/testcalculator/models.py
+++ djangoapp/testcalculator/models.py
@@ -3,19 +3,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class institution(models.Model):
+    name = models.CharField(null=False,unique=True,max_length=240)
+
 class user(models.Model):
     user_No = models.AutoField(primary_key=True)
     user_ID = models.CharField(null=False,unique=True,max_length=24)
     name = models.CharField(max_length=240)
     # TODO: Validate the email field
     email = models.CharField(null=False,unique=True,max_length=240)
-    # TODO: Convert institution to a foreign key
-    institution = models.CharField(max_length=240)
+    organization = models.ForeignKey(institution,default=1)
     # TODO: ALlow null here
     trainee_stage = models.CharField(max_length=240)
-
-class institution(models.Model):
-    name = models.CharField(null=False,unique=True,max_length=240)
 
 class test(models.Model):
     id = models.AutoField(primary_key=True)

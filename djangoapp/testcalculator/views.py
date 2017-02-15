d9aed4cc3f97f6b977980aae889f1f176f1960a9
Naveen Muthu <muthun@email.chop.edu>
2017-02-15 17:01:51 -0500

    Making institution foreign key

--- djangoapp/testcalculator/views.py
+++ djangoapp/testcalculator/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+
+def home(request):
+    html = "<!DOCTYPE html><html><head></head><body>Hello Django</body></html>"
+    return HttpResponse(html)

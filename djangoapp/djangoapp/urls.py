d62c0ba71c811a0983640e124a3f49886f948fc6
Naveen Muthu <muthun@email.chop.edu>
2017-03-30 10:04:08 -0400

    Working base website

--- djangoapp/djangoapp/urls.py
+++ djangoapp/djangoapp/urls.py
@@ -19,5 +19,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', views.home),
+    url(r'^home/', views.home),
+    url(r'^about/', views.about),
+    url(r'^references/', views.references),
     url(r'^admin/', admin.site.urls),
 ]
--- djangoapp/testcalculator/views.py
+++ djangoapp/testcalculator/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def home(request):
-    html = "<!DOCTYPE html><html><head></head><body>Hello Django</body></html>"
-    return HttpResponse(html)
+
+    return render(request,'index.html')
+
+def about(request):
+
+    return render(request,'about.html')
+
+def references(request):
+
+    return render(request,'reference.html')

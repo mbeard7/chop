3d3a129ab4156660cd15034a3e68d3a3842637a6
Naveen Muthu <muthun@email.chop.edu>
2017-07-20 23:45:17 -0400

    Correct user creation form and admin now has all of user profile

--- djangoapp/testcalculator/forms.py
+++ djangoapp/testcalculator/forms.py
@@ -1,12 +1,20 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
+insChoices = (
+    ('CHOP', "Childrens Hospital of Philadelphia"),
+    ('CCHMC', "Cincinnati Children's Hospital Medical Center"),
+    ('Other', "Other"),
+)
+
 class SignUpForm(UserCreationForm):
+    institution = forms.ChoiceField(choices=insChoices)
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = ('first_name', 'last_name', 'email', 'username', 'password1', 'password2', 'institution')
         help_texts = {
             'username': _('Letters, digits and @/./+/-/_ only.')
         }
--- djangoapp/testcalculator/models.py
+++ djangoapp/testcalculator/models.py
@@ -1,8 +1,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+class profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    institution = models.TextField(max_length=75, blank=False)
 
 class test(models.Model):
     id = models.AutoField(primary_key=True)
@@ -12,3 +19,9 @@ class test(models.Model):
     # TODO: ALlow null here
     synonym = models.CharField(max_length=240)
     cost = models.IntegerField(null=False)
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        profile.objects.create(user=instance)
+    instance.profile.save()
